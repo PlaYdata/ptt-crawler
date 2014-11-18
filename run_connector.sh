73fb11e9a870cbe3306bbcb5138cb927a9815f71
c3h3 <c3h3.tw@gmail.com>
2014-11-19 05:53:06 +0800

    run with WORKER_NAME from env. variable

--- run_connector.sh
+++ run_connector.sh
@@ -1,1 +1,1 @@
-celery worker -n "connector" -A ptt_crawler -Q ptt_pages -l info
+celery worker -n $WORKER_NAME -A ptt_crawler -Q ptt_pages -l info
--- run_parser.sh
+++ run_parser.sh
@@ -1,1 +1,1 @@
-celery worker -n "parser" -A ptt_crawler -Q ptt_posts -l info
+celery worker -n $WORKER_NAME -A ptt_crawler -Q ptt_posts -l info
